85448c7cbce8d3432c0d312e58f0a55400665bb0
alexandre-guerra <alexsguerra@gmail.com>
2023-10-31 11:51:16 -0300

    feito

--- country_pages.py
+++ country_pages.py
@@ -11,47 +11,59 @@ def load_saldo(df_pais):
     st.metric(label=f"Saldo", value="{:,.0f}".format(metrica))
 
 def conclusion():
-    st.title("Análise de Viabilidade de Exportação de Vinhos")
 
-    st.subheader("Dados e Análise - Estados Unidos")
-    st.write("""
-    Com base nos gráficos e dados fornecidos, aqui está uma análise preliminar:
+    st.text(" ")
+    st.subheader("Análise de Viabilidade de Exportação de Vinhos Brasileiros", divider=True)
+    st.text(" ")
+    st.markdown("""   
+    ##### Produção
+    A produção de vinhos teve uma queda significativa em 2016, a safra de uvas teve uma das maiores quebras registradas, com uma redução de 57% em relação ao ano anterior, equivalente, aproximadamente, a 300 milhões de quilos de uvas no território gaúcho. A principal causa da quebra histórica no ano 2016 foi uma série de acontecimentos climáticos que prejudicaram o desenvolvimento das uvas ao longo do ano, como geadas e excesso de chuvas, mas mostrou uma recuperação nos anos seguintes, especialmente entre 2017 e 2022.
 
-    1. **Comparativo de Dados Anuais**: 
-        - **Consumo**: O consumo tem sido bastante estável ao longo dos anos. 
-        - **Produção**: A produção parece ter aumentado nos primeiros anos e depois se estabilizou.
-        - **Exportação**: A exportação teve altos e baixos ao longo dos anos, mas nota-se uma queda acentuada em 2020 e 2022.
+    ##### Exportação
+    A quantidade de vinhos exportados apresentou variações ao longo dos anos. Em 2022, a quantidade de vinho exportado foi de 7.025.983 de litros, um número inferior ao pico recente de 8.058.704 de litros em 2021, mas ainda superior ao de muitos anos anteriores.
 
-    2. **Importações do Brasil por Ano**: 
-        - Houve uma diminuição geral nas importações ao longo dos anos, com algumas oscilações. A maior importação foi em 2008 com 440k e a menor em 2022 com 110k.
+    ##### Taxa de câmbio (PTAX)
+    A taxa PTAX aumentou consistentemente desde 2008 até 2021, indicando uma desvalorização da moeda local frente ao dólar. Isso pode tornar as exportações mais atrativas, pois os produtos nacionais ficam mais competitivos em termos de preço. Em 2022, houve uma pequena retração nessa taxa.
 
-    3. **Projeção de Inflação**:
-        - Espera-se que a inflação diminua nos próximos anos, o que pode favorecer o poder de compra dos consumidores.
-
-    4. **Saldo Anual com Linha de Tendência**:
-        - O saldo foi negativo na maioria dos anos, o que indica que o valor das importações superou o das exportações. No entanto, o saldo está melhorando ao longo dos anos, indicando uma tendência positiva.
-
-    5. **Dados Adicionais**:
-        - **Consumo Total**: 46,494,800,000
-        - **Importação Total**: 16,918,300,000
-        - **Produção Total**: 34,792,400,000
-        - **Exportação Total**: 5,713,200,000
-        - **Saldo**: -497,300,000
-
-    Conclusão:
-    O mercado brasileiro de vinhos apresenta um consumo estável e uma produção considerável. No entanto, as exportações são relativamente baixas em comparação com a produção, e o saldo é negativo, indicando que as importações superam as exportações.
-
-    Ao considerar a exportação de vinhos para os EUA, é importante avaliar:
-    - A qualidade e diferenciação dos vinhos brasileiros em relação aos vinhos já disponíveis nos EUA.
-    - Os custos de exportação e as tarifas de importação nos EUA.
-    - O potencial de marketing e a aceitação dos consumidores americanos.
-
-    Dada a projeção de diminuição da inflação, pode haver uma oportunidade de aumentar a competitividade no mercado americano, mas é essencial considerar todos os fatores mencionados acima antes de tomar uma decisão.
+    #### Conclusão
+    Considerando a recuperação na produção e a taxa de câmbio ainda elevada (apesar da pequena retração em 2022), pode ser um momento viável para entrar no mercado de exportação de vinhos. A desvalorização da moeda local pode ajudar a tornar os vinhos brasileiros mais competitivos em termos de preço no mercado internacional.
     """)
+    st.divider()
+
+    st.text(" ")
+    st.subheader("Prospecções futuras e possíveis ações para uma melhoria", divider=True)
+    st.text(" ")
+
+
+    st.markdown("""  
+    ##### USD por Litro:
+    Países como Reino Unido e Países Baixos têm um valor de USD por litro mais alto, indicando que eles podem valorizar vinhos de qualidade superior ou que há espaço para aumentar os preços nesses mercados.
+
+    ##### Principais Destinos:
+    Paraguai é o maior importador em termos de volume, mas o preço por litro é mais baixo. Países como Estados Unidos e Reino Unido importam menos em volume, mas o valor em USD é mais alto.
+
+    ##### Segmentação de Mercado:
+    Para mercados que pagam mais por litro, como Reino Unido e Países Baixos, pode-se focar em vinhos premium ou de edição limitada.
+
+    ##### Promoções e Degustações:
+    Em países com grande volume mas valor menor por litro, como o Paraguai, poderiam ser organizadas promoções e degustações para aumentar a percepção de valor do vinho brasileiro.
+
+    ##### Parcerias:
+    Estabelecer parcerias com distribuidores locais ou participar de feiras internacionais de vinho pode ajudar a aumentar a visibilidade e aceitação dos vinhos brasileiros nesses mercados.
+
+    ##### Branding e Marketing:
+    Investir em branding para posicionar o vinho brasileiro como uma escolha de qualidade e valor, destacando características únicas e diferenciais em relação a outros vinhos no mercado.
+
+    ##### Certificações:
+    Obter certificações de qualidade ou sustentabilidade pode aumentar a confiança e aceitação nos mercados internacionais.
+                    """)
+    st.divider()
 
 
 def Bibliography():
-    st.header("Bibliografia")
+    st.text(" ")
+    st.subheader("Bibliografia", divider=True)
+    st.text(" ")
 
     st.write("""
     Embrapa Uva e Vinho. (10/10/2023). Vitibrasil. http://vitibrasil.cnpuv.embrapa.br
@@ -76,7 +88,7 @@ def Bibliography():
 
     Organização Internacional da Vinha e do Vinho (OIV). (15/10/2023). Estatísticas. https://www.oiv.int/what-we-do/statistics
     """)
-
+    st.divider()
 
 def load_country_page(pais):
 
