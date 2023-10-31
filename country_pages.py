448db757e74aed45f996b0987e560482235c66fd
alexandre-guerra <alexsguerra@gmail.com>
2023-10-31 12:06:07 -0300

    chega

--- country_pages.py
+++ country_pages.py
@@ -15,48 +15,68 @@ def conclusion():
     st.text(" ")
     st.subheader("Análise de Viabilidade de Exportação de Vinhos Brasileiros", divider=True)
     st.text(" ")
-    st.markdown("""   
+    st.markdown(f"""  
     ##### Produção
     A produção de vinhos teve uma queda significativa em 2016, a safra de uvas teve uma das maiores quebras registradas, com uma redução de 57% em relação ao ano anterior, equivalente, aproximadamente, a 300 milhões de quilos de uvas no território gaúcho. A principal causa da quebra histórica no ano 2016 foi uma série de acontecimentos climáticos que prejudicaram o desenvolvimento das uvas ao longo do ano, como geadas e excesso de chuvas, mas mostrou uma recuperação nos anos seguintes, especialmente entre 2017 e 2022.
-
+    
+    """)
+    st.text(" ")
+    st.markdown(f"""  
     ##### Exportação
     A quantidade de vinhos exportados apresentou variações ao longo dos anos. Em 2022, a quantidade de vinho exportado foi de 7.025.983 de litros, um número inferior ao pico recente de 8.058.704 de litros em 2021, mas ainda superior ao de muitos anos anteriores.
-
+    """)
+    st.text(" ")
+    st.markdown(f"""  
     ##### Taxa de câmbio (PTAX)
     A taxa PTAX aumentou consistentemente desde 2008 até 2021, indicando uma desvalorização da moeda local frente ao dólar. Isso pode tornar as exportações mais atrativas, pois os produtos nacionais ficam mais competitivos em termos de preço. Em 2022, houve uma pequena retração nessa taxa.
-
+    """)
+    st.text(" ")
+    st.markdown(f"""  
     #### Conclusão
     Considerando a recuperação na produção e a taxa de câmbio ainda elevada (apesar da pequena retração em 2022), pode ser um momento viável para entrar no mercado de exportação de vinhos. A desvalorização da moeda local pode ajudar a tornar os vinhos brasileiros mais competitivos em termos de preço no mercado internacional.
     """)
-    st.divider()
 
+    st.divider()
     st.text(" ")
     st.subheader("Prospecções futuras e possíveis ações para uma melhoria", divider=True)
     st.text(" ")
 
 
     st.markdown("""  
-    ##### USD por Litro:
+    ##### USD por Litro
     Países como Reino Unido e Países Baixos têm um valor de USD por litro mais alto, indicando que eles podem valorizar vinhos de qualidade superior ou que há espaço para aumentar os preços nesses mercados.
-
-    ##### Principais Destinos:
+    """)
+    st.text(" ")
+    st.markdown(f"""  
+    ##### Principais Destinos
     Paraguai é o maior importador em termos de volume, mas o preço por litro é mais baixo. Países como Estados Unidos e Reino Unido importam menos em volume, mas o valor em USD é mais alto.
-
-    ##### Segmentação de Mercado:
+    """)
+    st.text(" ")
+    st.markdown(f"""  
+    ##### Segmentação de Mercado
     Para mercados que pagam mais por litro, como Reino Unido e Países Baixos, pode-se focar em vinhos premium ou de edição limitada.
-
-    ##### Promoções e Degustações:
+    """)
+    st.text(" ")
+    st.markdown(f"""  
+    ##### Promoções e Degustações
     Em países com grande volume mas valor menor por litro, como o Paraguai, poderiam ser organizadas promoções e degustações para aumentar a percepção de valor do vinho brasileiro.
-
-    ##### Parcerias:
+    """)
+    st.text(" ")
+    st.markdown(f"""  
+    ##### Parcerias
     Estabelecer parcerias com distribuidores locais ou participar de feiras internacionais de vinho pode ajudar a aumentar a visibilidade e aceitação dos vinhos brasileiros nesses mercados.
-
-    ##### Branding e Marketing:
+    """)
+    st.text(" ")
+    st.markdown(f"""  
+    ##### Branding e Marketing
     Investir em branding para posicionar o vinho brasileiro como uma escolha de qualidade e valor, destacando características únicas e diferenciais em relação a outros vinhos no mercado.
-
-    ##### Certificações:
+    """)
+    st.text(" ")
+    st.markdown(f"""  
+    ##### Certificações
     Obter certificações de qualidade ou sustentabilidade pode aumentar a confiança e aceitação nos mercados internacionais.
                     """)
+    
     st.divider()
 
 
